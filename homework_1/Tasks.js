5b489146f1c917eba76140b1d222f768deb96618
Denysov Maksym <93475429+Javac-g@users.noreply.github.com>
2023-08-28 21:04:27 +0300

    Update Tasks.js

--- homework_1/Tasks.js
+++ homework_1/Tasks.js
@@ -8,31 +8,31 @@ alert(put);
 //Task № 2
 ///////////////////////
 function myAge() {
-  let year = 2020 - prompt('год рождения')
+  let year = 2020 - prompt('birth year')
   return  year
 }
  alert( myAge())
 
 //Task № 3
 ///////////////////////
-let side = prompt('Длина стороны');
+let side = prompt('side length');
 let sq = side ** 2;
 let pr = side * 4;
 let g = sq + ' / ' + pr;
-alert('площадь / периметр =' +  g);
+alert('square / area =' +  g);
 
 //Task № 4
 ///////////////////////
-let r = prompt('Радиус окружности');
+let r = prompt('radius');
 let p =   Math.PI * r ** 2;
-alert('площадь =' +  p);
+alert('square =' +  p);
 
 //Task № 5
 ///////////////////////
-let s = prompt('Расстояние.км');
-let t = prompt('желаемое время в пути.Час');
+let s = prompt('Distanse.km');
+let t = prompt('Time');
 let v = s / t;
-alert('требуемая скорость равна =' + v + 'км / час' );
+alert('Speed =' + v + 'km / hour' );
 
 //Task № 6
 ///////////////////////
@@ -40,25 +40,25 @@ let usd = prompt('$');
 const ua =  28.36;
 const euro = 0.85;
 let conv = (usd * ua) + ' / ' +  (usd * euro);
-alert('Гривен/Евро=' + conv  );
+alert('uah/eur=' + conv  );
 
 //Task № 7
 ///////////////////////
 const usb = prompt('Размер флешки Гб');
 const file = 820;
 const prop = Math.trunc((usb * 1024) / file);
-alert('На флешке поместиться_' + prop + '_файлов');
+alert('Fit on flash drive_' + prop + '_files');
 
 //Task № 8
 ///////////////////////
-let money = prompt('Cумма денег в кошельке');
-let price = prompt('Цена одной шоколадки');
+let money = prompt('Money sum');
+let price = prompt('1 chocolate prise');
 let numb = Math.trunc(money / price);
 let oth = money - (price * numb) ;
 let res = numb + '/' + oth;
-alert(' шоколадок/Сдача =' + res);
+alert(' chocolates/change =' + res);
 
 //Task № 9
 ///////////////////////
-const number = prompt('Введите трехзначное число:');
+const number = prompt('enter 3 digit number:');
 alert(('' + number).split('').reverse().join(''));
